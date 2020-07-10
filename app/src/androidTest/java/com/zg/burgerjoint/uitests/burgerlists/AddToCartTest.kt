f9d86d0a0dbd9516951ca4dbebd741cdcaacce59
Myo Thiha <luciferthiha881997@gmail.com>
2020-07-10 21:00:26 +0630

    add login test

--- app/src/androidTest/java/com/zg/burgerjoint/uitests/burgerlists/AddToCartTest.kt
+++ app/src/androidTest/java/com/zg/burgerjoint/uitests/burgerlists/AddToCartTest.kt
@@ -1,10 +1,19 @@
 package com.zg.burgerjoint.uitests.burgerlists
 
 import android.content.Intent
+import android.view.View
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
 import androidx.test.rule.ActivityTestRule
+import com.zg.burgerjoint.R
 import com.zg.burgerjoint.activities.MainActivity
+import com.zg.burgerjoint.uitests.utils.first
 import org.junit.Before
+import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4ClassRunner::class)
@@ -16,6 +25,18 @@ class AddToCartTest {
     fun setUp(){
         activityTestRule.launchActivity(Intent())
     }
+
+    @Test
+    fun tapAddToCart_numberOfItemsInCartIncreased() {
+
+        onView(first<View>(withId(R.id.btnAddToCart)))
+            .perform(ViewActions.click())
+
+        onView(withId(R.id.tvCartCount))
+            .check(ViewAssertions.matches(ViewMatchers.withText("1")))
+    }
+
+
 
 
 
